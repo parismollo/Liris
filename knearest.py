ff26a44ba9a15d937a05625a7de3a48255372471
Paris <parismollochristondis@gmail.com>
2019-11-11 21:12:54 +0100

    majority vote

--- knearest.py
+++ knearest.py
@@ -9,3 +9,16 @@ def raw_majority_vote(labels: List[str]) -> str:
     return winner
 
 assert raw_majority_vote(['a','b','c','b']) == 'b'
+
+#  So we will reduce k until it finds only one winner
+
+def majority_vote(labels: List[str]) -> str:
+    vote_counts = Counter(labels)
+    winner, winner_count = vote_counts.most_common(1)[0]
+    num_winner = len([count for count in vote_counts.values() if count == winner_count])
+    if num_winner == 1:
+        return winner
+    else:
+        return majority_vote(labels[:-1])
+
+assert majority_vote(['a', 'b', 'c', 'b', 'a']) == 'b'
